d48416105143ffd756302bd88729725c20fdcbf6
Gabriel Fonseca <gabriel.silva@calindra.com.br>
2025-09-02 15:06:14 -0300

    chore: eitri conf

---
+++ eitri-app.conf.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "eitri-poc-ai",
+  title: "eitri-poc-ai",
+  slug: "eitri-poc-ai",
+  "eitri-luminus": "2.4.1",
+  "eitri-bifrost": "3.10.0",
+  "eitri-commons": "2.1.2",
+  "eitri-agents": "1.3.0",
+  version: "0.1.24",
+  "public-key": "12869e02-4605-490b-8539-dc730d17281e",
+  applicationId: "dc690c9d-aa0e-48c1-8fb2-f8e525529249",
+  id: "9ce99fa0-9557-4e53-ba25-1256dfd21c34",
+  organizationId: "749489a4-f129-4461-9da1-4b2d2a38dd5e",
+  organization: {},
+  "eitri-app-dependencies": {
+    i18n: {
+      version: "14.1.2",
+    },
+    "eitri-shopping-vtex-shared": { version: "1.3.4", isEitriAppShared: true },
+  },
+  type: "module",
+  "check-js": false,
+};
